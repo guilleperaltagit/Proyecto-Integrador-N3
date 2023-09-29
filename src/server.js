e3ca06d0d9b7020e2b41430ace62e0c0c75b76f3
guilleperaltagit <guilleep@hotmail.com>
2023-09-28 22:29:44 -0300

    Version con control de peticiones erroneas

--- src/server.js
+++ src/server.js
@@ -14,78 +14,108 @@ server.use(express.json());
 
 //Listado de Categorias
 server.get("/categorias", async (req, res) =>  {
-    const categorias = await Categorias.findAll()
-    res.status(200).send(categorias);
-})
+    try {
+        const categorias = await Categorias.findAll()
+        res.status(200).send(categorias)
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send(err)
+}})
 
 // Listado de todas las películas
 server.get("/catalogo", async (req, res) =>  {
-    // const products = await sequelize.query("select * from Products")
-
-    const peliculas = await Catalogo.findAll()
-    // Concateno la url a poster    
-    peliculas.forEach((i) => {i.poster = "http://dummyurl" + i.poster})
-
-    // const pricegte = Number(req.query.pricegte)
-    // const products = await Products.findAll({attributes: ["ProductName", "UnitPrice"]})
-
-    // const pricegte = Number(req.query.pricegte)
-    // const priceorder = req.query.priceorder
-    // const products = await Products.findAll({
-    //     attributes: ["ProductName", "UnitPrice"],
-    //     where: { UnitPrice: { [Op.gte]: pricegte} },
-    //    order: [ ['UnitPrice', priceorder] ] })
-   
-    res.status(200).send(peliculas);
-})
+    try {
+        const peliculas = await Catalogo.findAll()
+        // Concateno la url a poster    
+        peliculas.forEach((i) => {i.poster = "http://dummyurl" + i.poster})
+        res.status(200).send(peliculas)  
+    } catch (err){
+        console.log(err.message)
+        res.status(500).send(err)
+}})
 
 // Pelicula por ID
 server.get("/catalogo/:idpelicula", async (req, res) =>  {
-    // const product = await Products.findOne({where: { ProductId : { [Op.eq]: req.params.productid } } })
-    // const product = await Products.findOne({where: { ProductId : req.params.productid }})
+    //Valido si idpelicula es un número y no es cero
+    const ID = Number(req.params.idpelicula)
+    const esString = isNaN(ID)
+    if (esString === true || ID === 0){
+        res.status(400).send({message: "El ID debe ser un número y no puede ser cero"})
+        return
+    }
 
-    const pelicula = await Catalogo.findByPk(req.params.idpelicula)
-    // Concateno la url a poster
-    pelicula.poster = "http://dummyurl" + pelicula.poster
-    res.status(200).send(pelicula);
-})
+    try {
+        const pelicula = await Catalogo.findByPk(req.params.idpelicula)
+        console.log(pelicula)
+        // Valido si idpelicula trae una pélicula válida del catálogo
+        if (!pelicula) {
+            res.status(400).send({message: "El ID debe ser un número válido del catálogo"})
+            return
+        }
+        // Concateno la url a poster
+        pelicula.poster = "http://dummyurl" + pelicula.poster
+        res.status(200).send(pelicula)
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send(err.message)
+}})
 
 // Listado de películas filtrado por nombre
-server.get("/catalogo/nombre/:nombre", async (req, res) =>  {
-    const peliculas = await Catalogo.findAll({
-        where: { titulo: { [Op.substring]: req.params.nombre}}
-    })
-    // Concateno la url a poster    
-    peliculas.forEach((i) => {i.poster = "http://dummyurl" + i.poster})
-    res.status(200).send(peliculas);
-})
+server.get("/catalogo/nombre/:nombre", async (req, res) => {
+    try {
+        const peliculas = await Catalogo.findAll({
+            where: { titulo: { [Op.substring]: req.params.nombre}}
+        })
+        // Concateno la url a poster    
+        peliculas.forEach((i) => {i.poster = "http://dummyurl" + i.poster})
+        res.status(200).send(peliculas);        
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send(err.message)
+}})
 
 // Listado de películas filtrado por género
 server.get("/catalogo/genero/:genero", async (req, res) =>  {
-    const peliculas = await Peliculaygenero.findAll({
-        where: { genero: req.params.genero}
-    })
-    // Concateno la url a poster
-    peliculas.forEach((i) => {i.poster = "http://dummyurl" + i.poster})
-    res.status(200).send(peliculas);
-})
+    try {
+        const peliculas = await Peliculaygenero.findAll({
+            where: { genero: req.params.genero}
+        })
+        if (peliculas.length === 0) {
+            res.status(400).send({message: "El género no es válido. Ingrese un género válido"})
+            return
+        }
+        // Concateno la url a poster
+        peliculas.forEach((i) => {i.poster = "http://dummyurl" + i.poster})
+        res.status(200).send(peliculas)
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send(err.message)
+}})
 
 // Listado de películas filtrado por categoría
 server.get("/catalogo/categoria/:categoria", async (req, res) =>  {
+    try {
     const peliculas = await Catalogo.findAll({
         where: { categoria: req.params.categoria }
     })
+    if (peliculas.length === 0) {
+        res.status(400).send({message: "La categoría no es válida. Ingrese un categoría válida"})
+        return
+    }
     // Concateno la url a poster
     peliculas.forEach((i) => {i.poster = "http://dummyurl" + i.poster})
     res.status(200).send(peliculas);
-})
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send(err.message)
+}})
 
-// Control de rutas inexistentes
+// Manejo de rutas inexistentes
 server.use('*', (req, res) => {
-    res.status(404).send({error: `La URL indicada no existe en este servidor`});
+    res.status(404).send({error: "La URL solicitada no existe en este servidor"});
 });
 
-// Método oyente de solicitudes
+// Escucha de peticiones
 sequelize.authenticate().then(()=>{
     sequelize.sync({ force: false }).then(()=>{
         server.listen(process.env.PORT, process.env.HOST, () => {
